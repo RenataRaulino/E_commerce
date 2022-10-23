f9a0ac791a370b58575f47f83cf72c62885ecfc9
Renata <renataraulino2009@gmail.com>
2022-10-23 08:04:19 -0300

    Atualizações

--- src/main/java/br/com/residencia/ecommerce/entity/ItemPedido.java
+++ src/main/java/br/com/residencia/ecommerce/entity/ItemPedido.java
@@ -13,7 +13,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import javax.persistence.Table;
-//import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+
 //import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 //@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "idItemPedido")
